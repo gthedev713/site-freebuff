91e4ec97cd2cb5edcdb88e5afdf1e73845997591
gthedev713 <gkoulalis081@hotmail.com>
2026-07-08 17:47:38 +0300

    Create NotFound.tsx

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+// src/pages/NotFound.tsx
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">404</h1>
+      <p>Η σελίδα που ψάχνεις δεν υπάρχει.</p>
+    </div>
+  );
+}
